7482b415133080123ccce4507a0a3d2a8a028cc8
Bruno Wollmann <bruno@terra-comm.ca>
2019-12-05 23:39:58 -0600

    Completed exercise 2.
    Used for loop which hasn't been covered yet.
    Did this to keep the print statements shorter.

--- KB/lesson1/exercise2.py
+++ KB/lesson1/exercise2.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python
+
+ip_addr = input('Please enter an IP address: ')
+
+octets = ip_addr.split('.')
+octets_bin = []
+octets_hex = []
+
+for x in octets:
+	octets_bin.append(bin(int(x)))
+	octets_hex.append(hex(int(x)))
+
+header = ['Octet1', 'Octet2', 'Octet3', 'Octet4']
+
+print ()
+print ('{:^15}{:^15}{:^15}{:^15}'.format(*header))
+print ('-' * 60)
+print ('{:^15}{:^15}{:^15}{:^15}'.format(*octets))
+print ('{:^15}{:^15}{:^15}{:^15}'.format(*octets_bin))
+print ('{:^15}{:^15}{:^15}{:^15}'.format(*octets_hex))
+print ('-' * 60)
+print ()
