c745162763cccccac29d68aeee5d4bee3df3ad1f
Bruno Wollmann <bruno@terra-comm.ca>
2020-01-02 14:16:13 -0600

    Completed Exercise 5

--- KB/lesson3/exercise5.py
+++ KB/lesson3/exercise5.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python
+
+import os
+
+WINDOWS = False
+base_cmd_linux = 'ping -c 2'
+base_cmd_windows = 'ping -n 2'
+base_cmd = base_cmd_windows if WINDOWS else base_cmd_linux
+
+print('\n')
+base = '.'
+while base.count('.') < 3:
+    base = input('Enter the base /24 network: ')
+
+network_range = []
+for x in range(1, 255):
+    network_range.append(base + str(x))
+
+print('\n')
+for ind, ip in enumerate(network_range):
+    print('{} ---> {}'.format(ind, ip))
+
+print('\n')
+
+ping_range = network_range[2:6]
+for host in ping_range:
+    os.system('{} {}'.format(base_cmd, host))
+
+print('\n')
