8969b8e76606aa1e52f79153ab347a76dfe406b2
Bruno Wollmann <bwollmann@saskpower.com>
2020-04-22 21:02:37 -0600

    Complete exercise 1

--- KB/lesson6/exercise1.py
+++ KB/lesson6/exercise1.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 
+from getpass import getpass
+from netmiko import ConnectHandler
+
+username = input('Username: ')
+password = getpass()
+
+switch_1 = {
+    'device_type': 'cisco_nxos',
+    'host': '172.31.1.1',
+    'username': username,
+    'password': password,
+}
+
+net_connect = ConnectHandler(**switch_1)
+output = net_connect.find_prompt()
+print('-' * 40)
+print(output)
