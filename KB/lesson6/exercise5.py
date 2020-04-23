50317262c787ac26834ab4ce100746802e37da43
Bruno Wollmann <bwollmann@saskpower.com>
2020-04-22 21:58:20 -0600

    Complete exercise 5

--- KB/lesson6/exercise5.py
+++ KB/lesson6/exercise5.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python
 
+from getpass import getpass
+from netmiko import ConnectHandler
+
+print('\n')
+
+username = input('Username: ')
+password = getpass()
+
+switch_1 = {
+    'device_type': 'cisco_nxos',
+    'host': '172.31.1.1',
+    'username': username,
+    'password': password,
+}
+
+cmd = 'show ip arp'
+
+net_connect = ConnectHandler(**switch_1)
+output = net_connect.send_command(cmd, use_textfsm=True)
+net_connect.disconnect()
+
+print('-' * 40)
+print(output)
